bb7721e75f86c3479b655c134a4b663dbcc067e0
gongas13 <gpinheiro93@gmail.com>
2016-05-20 19:10:55 +0100

    wefefw

--- server.c
+++ server.c
@@ -83,22 +83,34 @@ void backupficheiro(char *ficheiro, char *shaisum)
 
 void restoreficheiro(char *ficheiro)
 {
-    char *dat, *meta;
+    char *dat, *meta, actual[200];
     int x,status,pid[4],i=0;
     meta=getPathOfMetadataFolder();
-    x=readlink(meta,dat,20);
+
+    getcwd(actual,sizeof(actual));
+
+    strcat(meta,ficheiro);
+
+    printf("actual: %s\n",actual );
+    printf("meta: %s\n",meta );
+
+    x=readlink(meta,dat,128);
+    printf("dat: %s\n",dat );
+
 
     if(pid[i++]=fork()==0) {
-      execlp("cp","cp",ficheiro,dat, NULL);
+      printf("copiar\n");
+      execlp("cp","cp",dat,actual, NULL);
 
     } else {
       waitpid(pid[0],&status,0);
 
         if (pid[i++]=fork()==0) {
-        execlp("gunzip","gunzip",ficheiro);
+          printf("unzipar\n");
+          execlp("gunzip","gunzip",ficheiro);
         }
-        else{
-          waitpid(pid[1],&status,0);
+          else{
+            waitpid(pid[1],&status,0);
         }
     }
 }
@@ -184,7 +196,7 @@ the public FIFO every time a client process finishes its activities.
         }
         else if(strcmp(comando, "restore") == 0)
         {
-            
+
         }
     }
     unlink(fifo);
